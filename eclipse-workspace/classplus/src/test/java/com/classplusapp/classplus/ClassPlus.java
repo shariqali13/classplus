d826ccd8137cf5361607541096e5fb0ae2c96ecb
“shariqali13” <mohd.shariq@byo.social>
2019-01-21 03:39:47 +0530

    New Tests

--- eclipse-workspace/classplus/src/test/java/com/classplusapp/classplus/ClassPlus.java
+++ eclipse-workspace/classplus/src/test/java/com/classplusapp/classplus/ClassPlus.java
@@ -1,5 +1,7 @@
 package com.classplusapp.classplus;
 
+import static org.testng.Assert.assertEquals;
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -15,6 +17,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
@@ -26,7 +29,7 @@ import org.testng.annotations.Test;
 
 public class ClassPlus {
 	
-	WebDriver driver;
+	static WebDriver driver;
 
 	@BeforeTest
 	public void setUp () throws InterruptedException
@@ -45,12 +48,47 @@ public class ClassPlus {
 	
 	@Test
 	
+	public static void homePage()
+	{
+		
+	//driver.findElement(By.xpath("//h1[@class='font-body']"));
+		
+	String a=	driver.findElement(By.xpath("//h1[@class='customFont']")).getText();
+	
+	System.out.println(a);
+		String b= "A Single Portal for all your Activities";	
+	assertEquals(a,b);
+	}
+	
+	@Test
+	
 	public void titleVerify()
 	
 	{
 		String title = driver.getTitle();
 		System.out.println(title);
 	}
+	
+	@Test
+	
+	public static void productPage() throws InterruptedException
+	
+	{
+		
+		driver.findElement(By.xpath("//ul[@class='one-page-menu sf-js-enabled']//li//a[@href='product.html']")).click();
+	Thread.sleep(3000);
+	WebElement ImageFile = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/section[2]/div[1]/div[1]/div[1]/div[1]/div[1]/img[1]"));
+
+    Boolean ImagePresent = (Boolean) ((JavascriptExecutor)driver).executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", ImageFile);
+    if (!ImagePresent)
+    {
+         System.out.println("Image not displayed.");
+    }
+    else
+    {
+        System.out.println("Image displayed.");
+    }
+}
 	
 	@AfterTest
 	
